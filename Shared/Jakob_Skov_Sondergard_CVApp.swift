a8656894a09c4dd4ed599ab9996b5d7b475caa34
saxjax <saxjax@saxjax.dk>
2022-08-05 00:26:05 +0200

    Initial Commit

---
+++ Shared/Jakob_Skov_Sondergard_CVApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jakob_Skov_Sondergard_CVApp.swift
+//  Shared
+//
+//  Created by Jakob Skov Søndergård on 05/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Jakob_Skov_Sondergard_CVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
